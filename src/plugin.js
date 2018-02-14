875c89589cd6a15e9817b2d6b691a6de50c211c6
Michiel de Jong <michiel@ripple.com>
2018-02-14 14:48:58 +0100

    no opts.peerPublicKey

--- src/plugin.js
+++ src/plugin.js
@@ -22,8 +22,6 @@ class PluginLightning extends PluginBtp {
       throw new InvalidFieldsError('missing opts.lndTlsCertPath;' +
           ' try /home/YOURNAME/.lnd/tls.cert (Linux) or' +
           ' /Users/YOURNAME/Library/Application Support/Lnd/tls.cert (Mac)')
-    } else if (!opts.peerPublicKey) {
-      throw new InvalidFieldsError('missing opts.peerPublicKey')
     } else if (!opts.maxInFlight && !opts.maxUnsecured) {
       throw new InvalidFieldsError('missing opts.maxInFlight')
     } else if (!opts.lndUri) {
@@ -36,7 +34,6 @@ class PluginLightning extends PluginBtp {
     this.amountSettled = ctx.backend.getMaxValueTracker('amount_settled')
     this.maxUnsecured = opts.maxUnsecured || opts.maxInFlight
     this.authToken = opts.authToken
-    this.peerPublicKey = opts.peerPublicKey
     this.lndUri = opts.lndUri
 
     this.lndTlsCertPath = opts.lndTlsCertPath
